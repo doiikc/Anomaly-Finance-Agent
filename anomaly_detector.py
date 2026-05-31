3e5307446f21d7ebbf873213ad9d091133a954a7
doiikc <doga@kocyigit.org>
2026-05-31 18:18:35 +0200

    Logical checks added

--- anomaly_detector.py
+++ anomaly_detector.py
@@ -10,10 +10,20 @@ def data_fetcher(stockName, period="1y", interval="1d"):
     return data
 
 def detect_anomalies(data):
+    data['Status_Reason']="Normal Market Activity"
+    data['Is_Anomaly']=False
+
+    price_error= data['Close'] <=0
+    data.loc[price_error, 'Is_Anomaly']=True
+    data.loc[price_error, 'Status_Reason']= "Bad Data:Price cannot be zero or negative"
+
     data['Mean']= data['Close'].rolling(window=30).mean()
     data['Std']= data['Close'].rolling(window=30).std()
-
     data['Z_score']= (data['Close']-data['Mean'])/ data['Std']
 
-    data['Is_Anomaly']= data['Z_score'].abs() >3
+    z_error = (data['Z_score'].abs() > 3) & (data['Status_Reason'] == "Normal Market Activity")
+    #normal market activity so that it isnt a logical error
+    data.loc[z_error, 'Is_Anomaly'] = True
+    data.loc[z_error, 'Status_Reason'] = "Statistical Anomaly: Significant price movement"
     return data
+
